9eb037567487d76dfe495932044b8f9e76529123
Liberatone <musictus@gmail.com>
2019-01-19 22:35:06 -0500

    files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("In N Out");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Jack in the box");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Shake Shak");
